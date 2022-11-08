04c403214fbb64391baf0f42d2946dbd341337fe
ZaneLewes <tennis_forever@163.com>
2022-11-08 16:58:02 +0800

    rebuilding site Tue Nov  8 16:58:02     2022

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,12 +2,13 @@
 
 $(function () {
 	$.backstretch([
+		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655999.png",
 		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042110890.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042120208.png",
+		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655998.png",
 		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042142032.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042200996.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042256743.png"
-	], { duration: 10000, fade: 1500 });
+		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655996.png",
+		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655996.png"
+	], { duration: 1000, fade: 1500 });
 });
 
 
