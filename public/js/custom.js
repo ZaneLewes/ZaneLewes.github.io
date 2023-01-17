fce551117111a468797341b541597bd7b6d11b13
ZaneLewes <tennis_forever@163.com>
2023-01-17 08:29:47 +0800

    rebuilding site Tue Jan 17 08:29:47     2023

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,14 +1,1 @@
-/* 轮播背景图片 */
-
-$(function () {
-	$.backstretch([
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655999.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042110890.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655998.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211042142032.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655996.png",
-		"https://zanelewes.oss-cn-beijing.aliyuncs.com/img/202211081655996.png"
-	], { duration: 6666, fade: 1500 });
-});
-
 
